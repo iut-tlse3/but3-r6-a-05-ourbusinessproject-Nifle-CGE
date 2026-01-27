eba86022d3b3b1139e99c9e6af25b326405a0224
Nifle-CGE <pub@elfin.fr>
2026-01-27 14:23:30 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,10 +1,18 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Enterprise {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     @NotEmpty
     private String name;
 
@@ -17,6 +25,14 @@ public class Enterprise {
     @NotEmpty
     @Email
     private String contactEmail;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,11 +1,28 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     @NotEmpty
     private String title;
+
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
