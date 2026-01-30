3746d5b47fa2cf1f3558d65e7175981091f29480
Nifle-CGE <pub@elfin.fr>
2026-01-30 10:18:46 +0100

    fix #4.1 classe Partnership

---
+++ src/main/java/ourbusinessproject/Partnership.java
@@ -0,0 +1,41 @@
+package ourbusinessproject;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class Partnership {
+
+    @NotNull
+    private Date creationDate;
+
+    @NotNull
+    private Enterprise enterprise;
+
+    @NotNull
+    private Project project;
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+}
